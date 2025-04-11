a0c7eccd8c57109e635dcf22725669ff645338b0
Marcos Bião <dev.marcosdly@gmail.com>
2025-04-10 23:40:04 -0300

    feat(bases): HasChildren: add widgets as dict trees

--- simple_tkinter/bases.py
+++ simple_tkinter/bases.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 from uuid import UUID
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Union, cast
+from collections.abc import Iterable
 
 
 if TYPE_CHECKING:
     from simple_tkinter.app import App
     from simple_tkinter.widget import Widget
+
+
+WidgetTree = dict["Widget", Union["Widget", Iterable["Widget"], "WidgetTree"]]
 
 
 class HasChildren:
@@ -36,3 +40,28 @@ class HasChildren:
         widget.app.append_child(widget)
         if widget.parent is self:
             self._children.append(widget)
+
+    def append_children_tree(self, widget_tree: WidgetTree):
+        if not len(widget_tree):
+            return
+
+        from simple_tkinter.widget import Widget
+
+        for widget, tree_descriptor in widget_tree.items():
+            self.append_child(widget)
+            if isinstance(tree_descriptor, dict):
+                # recursion
+                self.append_children_tree(cast(WidgetTree, tree_descriptor))
+                continue
+            if isinstance(tree_descriptor, Widget):
+                self.append_child(tree_descriptor)
+                continue
+            _iterable = ()
+            try:
+                _iterable = iter(tree_descriptor)
+            except TypeError as err:
+                raise TypeError(
+                    "widget tree expected to be iterable but is not"
+                ) from err
+            for sub_widget in _iterable:
+                self.append_child(sub_widget)
