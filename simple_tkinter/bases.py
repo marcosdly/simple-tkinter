fd31f861c24f493c32349804f44950bff5407ac1
Marcos Bião <dev.marcosdly@gmail.com>
2025-04-12 13:33:59 -0300

    feat(bases): add HasPresenceInSpace base

--- simple_tkinter/bases.py
+++ simple_tkinter/bases.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
 
-from uuid import UUID
-from typing import TYPE_CHECKING, Union, cast, NamedTuple, TypeVar, Generic, Any, Callable
-from collections.abc import Iterable
 from simple_tkinter.types import UniqueSymbolValue
+
+from abc import abstractmethod
+from uuid import UUID
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Union,
+    Generic,
+    TypeVar,
+    Callable,
+    NamedTuple,
+    cast,
+)
+from dataclasses import field, asdict, astuple, replace, dataclass
+from collections.abc import Iterable
 
 
 if TYPE_CHECKING:
@@ -67,9 +79,12 @@ class HasChildren:
             for sub_widget in _iterable:
                 self.append_child(sub_widget)
 
+
 ES = TypeVar("ES", bound=NamedTuple)
 
+
 class EventValueUnknown(UniqueSymbolValue): ...
+
 
 class EventCallState(Generic[ES], NamedTuple):
     event_name: str
@@ -77,15 +92,17 @@ class EventCallState(Generic[ES], NamedTuple):
     event_data: Union[EventValueUnknown, Any]
     event_state: ES
     caller_widget: "Widget"
-    target_current:"Widget"
+    target_current: "Widget"
     target_current_is_self: bool
     target_by_id: Union["Widget", None]
     target_by_class: tuple["Widget", ...]
 
+
 EventCallback = Callable[[EventCallState[ES]], Any]
 
+
 class EventRegistry(dict[str, EventCallback[ES]]):
-    def __init__(self, mapping: dict[str, EventCallback[ES]], *, frozen:bool=False):
+    def __init__(self, mapping: dict[str, EventCallback[ES]], *, frozen: bool = False):
         super().__init__()
         for name, func in mapping.items():
             self[name] = func
@@ -93,20 +110,21 @@ class EventRegistry(dict[str, EventCallback[ES]]):
 
     def __getitem__(self, name: str):
         if self._frozen:
-            raise KeyError('object is frozen and can not be modified')
+            raise KeyError("object is frozen and can not be modified")
         return super()[name]
 
     def __setitem__(self, name: str, callback: EventCallback[ES]):
         if self._frozen:
-            raise KeyError('object is frozen and can not be modified')
+            raise KeyError("object is frozen and can not be modified")
         if not callable(callback):
-            raise TypeError('event callback must be callable')
+            raise TypeError("event callback must be callable")
         super()[name] = callback
 
     def __delitem__(self, name: str):
         if self._frozen:
-            raise KeyError('object is frozen and can not be modified')
+            raise KeyError("object is frozen and can not be modified")
         del super()[name]
+
 
 class HasEvents:
     events_reserved: EventRegistry[Any]
@@ -117,8 +135,74 @@ class HasEvents:
             self.events_reserved = EventRegistry(self.events_reserved, frozen=True)
         else:
             self.events_reserved = EventRegistry({}, frozen=True)
-        if hasattr(self,"events"):
+        if hasattr(self, "events"):
             self.events = EventRegistry(self.events)
         else:
             self.events = EventRegistry({})
 
+
+@dataclass(unsafe_hash=True)
+class DimensionBox:
+    x: float = field(default=0)
+    y: float = field(default=0)
+    z: int = field(default=0)
+    vec2d: tuple[float, float] = field(init=False, default=(0, 0))
+    vec3d: tuple[float, float, int] = field(init=False, default=(0, 0, 0))
+    vec2d_center: tuple[float, float] = field(init=False, default=(0, 0))
+    vec3d_center: tuple[float, float, int] = field(init=False, default=(0, 0, 0))
+    has_subpixel_position: bool = field(init=False, default=False)
+    w: float = field(default=0)
+    h: float = field(default=0)
+    has_subpixel_size: bool = field(init=False, default=False)
+    perimeter: float = field(init=False, default=0)
+    area: float = field(init=False, default=0)
+
+    def as_dict(self):
+        return asdict(self)
+
+    def as_tuple(self):
+        return astuple(self)
+
+    def copy_shallow(
+        self, **changes: Union[int, float, bool, tuple[Union[int, float], ...]]
+    ):
+        return replace(self, **changes)
+
+    @abstractmethod
+    def overlaps(self, other: "DimensionBox") -> bool: ...
+
+    @abstractmethod
+    def covers(self, other: "DimensionBox") -> bool: ...
+
+    @abstractmethod
+    def contains(self, other: "DimensionBox") -> bool: ...
+
+    @abstractmethod
+    def inside(self, other: "DimensionBox") -> bool: ...
+
+    @abstractmethod
+    def touches(self, other: "DimensionBox") -> bool: ...
+
+
+class HasPresenceInSpace:
+    def will_appearance_update(self) -> bool: ...
+
+    @abstractmethod
+    def get_box_absolute(
+        self,
+        *,
+        only_content: bool = False,
+        after_appearance_update: bool = False,
+        include_border: bool = True,
+        include_title_bar_if_window: bool = True,
+    ) -> DimensionBox: ...
+
+    @abstractmethod
+    def get_box_required(
+        self,
+        *,
+        only_content: bool = False,
+        after_appearance_update: bool = False,
+        include_border: bool = True,
+        include_title_bar_if_window: bool = True,
+    ) -> DimensionBox: ...
